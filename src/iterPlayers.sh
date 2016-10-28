f41476a6903401fb8320389dcc892feb62156f9e
tahmerjay <21130321@student.uwa.edu.au>
2016-10-28 21:41:18 +0800

    added stats directory stuff and a how to

--- src/iterPlayers.sh
+++ src/iterPlayers.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 players=5
-echo "percentage of times the Resistance Win" > $1.csv
-echo "each row is the average of 10 rounds of 100 games" >> $1.csv
-echo "min,max,avg" >> $1.csv
+echo "percentage of times the Resistance Win" > statistics/$1.csv
+echo "each row is the average of 10 rounds of 100 games" >> statistics/$1.csv
+echo "min,max,avg" >> statistics/$1.csv
 while [ $players -lt 11 ]; do
   ./rungame.sh 1 $players $1
   let players=players+1
 done
-rm out.txt
+rm statistics/out.txt
