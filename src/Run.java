36e48e357b919b7300458a4911175c056bf8cf16
Jack Scott <jack.rob.scott@gmail.com>
2016-10-29 15:51:17 +0800

    add in ErnieAI

--- src/Run.java
+++ src/Run.java
@@ -1,5 +1,6 @@
 import cits3001_2016s2.*;
 import s21504053.*;
+import s21130321.*;
 
 import java.util.*;
 import java.io.*;
@@ -13,9 +14,10 @@ public class Run{
       FileWriter fw = new FileWriter(f);
       Competitor[] contenders = {
         new Competitor(new cits3001_2016s2.RandomAgent(),"Randy","Tim"),
-        new Competitor(new s21504053.BoneCrusher(),"BoneCrusher","Jack Scott")
-        };
-      fw.write(Game.tournament(contenders, 500));
+        new Competitor(new s21504053.BoneCrusher(),"BoneCrusher","Jack"),
+        new Competitor(new s21130321.Ernie21130321(),"ErnieAI","Tahmer")
+      };
+      fw.write(Game.tournament(contenders, 5000));
       fw.close();
     }
     catch(IOException e){System.out.println("IO fail");}
